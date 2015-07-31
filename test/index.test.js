717e65bc73c74a53515899e36e7f169e357565e6
Remy Sharp <remy@remysharp.com>
2015-07-31 16:50:33 +0100

    fix: don't rewrite the exports
    BREAKING CHANGE: this means individual projects should export the result in their own config allowing the config to be loaded once and once only, but fixes the issue that the snyk-config was being loaded as a singleton (and thus caching the config for the first loaded - rather than letting many modules use the config loader).

--- test/index.test.js
+++ test/index.test.js
@@ -1,14 +1,14 @@
 var test = require('tape');
 
 test('can be loaded twice', function (t) {
-  var config = require('../')('fixtures');
+  var config = require('../')('fixtures/one');
 
   t.equal(config.foo, 1, 'config matches');
   t.equal(config.bar, 2, 'config matches');
 
-  var config2 = require('../');
-  t.equal(config2.foo, 1, 'config2 matches');
-  t.equal(config2.bar, 2, 'config2 matches');
+  var config2 = require('../')('fixtures/two');
+  t.equal(config2.foo, 10, 'config2 matches');
+  t.equal(config2.bar, 20, 'config2 matches');
 
   t.end();
 });
