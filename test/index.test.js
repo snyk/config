fdc3cc731e075ad783d01033b1eee260d9fb590a
Remy Sharp <remy@remysharp.com>
2015-08-04 18:19:16 +0100

    test: make sure we test env overrides

--- test/index.test.js
+++ test/index.test.js
@@ -12,3 +12,21 @@ test('can be loaded twice', function (t) {
 
   t.end();
 });
+
+test.only('env values override', function (t) {
+  process.env.SNYK_foo = 100; // jshint ignore:line
+  process.env.PORT = 8888; // jshint ignore:line
+  var config = require('../')('fixtures/one');
+
+  console.log(config);
+
+  t.equal(config.foo, 100, 'config matches');
+  t.equal(config.bar, 2, 'config matches');
+
+  t.end();
+});
+
+test('can be called without a path', function (t) {
+  t.ok(require('../')(), 'config loaded without a path');
+  t.end();
+});
